8d6963bc84d4bff987c5abbb58b2289a183995b0
robohooman <158758496+robohooman@users.noreply.github.com>
2024-03-05 03:41:59 +0700

    Add files via upload

---
+++ user/firebaseConfig.js
@@ -0,0 +1,23 @@
+
+import { initializeApp } from "firebase/app";
+import {getDatabase} from 'firebase/database'
+import {getStorage} from 'firebase/storage'
+const firebaseConfig = {
+  apiKey: "AIzaSyCc5ASHRE6cAY8KKBE44UTJaxGClq-ShqI",
+  authDomain: "mvs-news.firebaseapp.com",
+  databaseURL: "https://mvs-news-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "mvs-news",
+  storageBucket: "mvs-news.appspot.com",
+  messagingSenderId: "670401932144",
+  appId: "1:670401932144:web:c84a3b1e6c4b24ec967db5",
+  measurementId: "G-Q3MTFSB4HZ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+console.log(app);
+const database = getDatabase(app);
+console.log(database);
+const storage = getStorage(app);
+console.log('Storage:', storage);
+export {database, storage}
